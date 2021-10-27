6485a8b127cfa678ca3f22f873c2a46b74296485
akiko.kano <47952174+ichirin-planet@users.noreply.github.com>
2021-10-27 19:02:38 +0900

    Login

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,15 @@
+import { Box, Button, Divider, Flex, Heading, Input } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const Login: VFC = memo(() => {
-  return <p>ログインページ</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1">ユーザー管理アプリ</Heading>
+        <Divider />
+        <Input placeholder="ユーザーID" />
+        <Button>ログイン</Button>
+      </Box>
+    </Flex>
+  );
 });
