05c5c95d85980308cb710161e25ec7f23765c08a
akiko.kano <47952174+ichirin-planet@users.noreply.github.com>
2021-10-28 15:37:04 +0900

    selectedUser

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -10,14 +10,17 @@ import { memo, useCallback, useEffect, VFC } from "react";
 import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserDetailModal } from "../organisms/user/UserDetailModal";
+import { useSelectUser } from "../../hooks/useSelectUser";
 
 export const UserManagement: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, users, loading } = useAllUsers();
+  const { onSelectUser, selectedUser } = useSelectUser();
+  console.log(selectedUser);
   useEffect(() => getUsers(), []);
 
   const onClickUser = useCallback((id: number) => {
-    console.log(id);
+    onSelectUser({ id, users });
     onOpen();
   }, []);
   return (
